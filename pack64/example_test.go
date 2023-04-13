544e4efcca0c37169efc58a13fa3b059693890b8
Pascal S. de Kloe <pascal@quies.net>
2023-04-13 16:03:34 +0200

    example data tweak

--- pack64/example_test.go
+++ pack64/example_test.go
@@ -6,20 +6,21 @@ import (
 	"github.com/pascaldekloe/wordpack/pack64"
 )
 
+// Full-cycle demonstration.
 func Example_codec() {
 	var data = [64]int{
-		99, 100, -101, 1001, 0, 0, 0, 1,
-		99, 100, -101, 1001, 0, 0, 0, 2,
-		99, 100, -101, 1001, 0, 0, 0, 3,
-		99, 100, -101, 1001, 0, 0, 0, 4,
-		99, 100, -101, 1001, 0, 0, 0, 5,
-		99, 100, -101, 1001, 0, 0, 0, 6,
-		99, 100, -101, 1001, 0, 0, 0, 7,
-		99, 100, -101, 1001, 0, 0, 0, 8,
+		99, 100, -101, 1, 2, 3, 4, 5,
+		144, 145, 146, 147, 148, 149, 150, 151,
+		244, 245, 246, 247, 248, 249, 250, 251,
+		344, 345, 346, 347, 348, 349, 350, 351,
+		444, 445, 446, 447, 448, 449, 450, 451,
+		544, 545, 546, 547, 548, 549, 550, 551,
+		644, 645, 646, 647, 648, 649, 650, 651,
+		744, 745, 746, 747, 748, 749, 750, 1001,
 	}
 
 	pack := pack64.AppendDeltaEncode(nil, &data, data[0])
-	fmt.Printf("compressed %d integers into %d ✓\n", len(data), len(pack))
+	fmt.Printf("compressed %d integers into %d words ✓\n", len(data), len(pack))
 
 	var got [64]int
 	pack64.AppendDeltaDecode(got[:0], pack, 99)
@@ -27,6 +28,6 @@ func Example_codec() {
 		fmt.Println("got input back after codec cycle ✓")
 	}
 	// Output:
-	// compressed 64 integers into 12 ✓
+	// compressed 64 integers into 9 words ✓
 	// got input back after codec cycle ✓
 }
